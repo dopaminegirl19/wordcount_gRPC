46f967676876fd0e6c9191b59e36e51052f150e3
Morgane Moss <morganemoss@icloud.com>
2021-07-04 11:42:52 +0200

    stream map output

--- mapreduce_server.py
+++ mapreduce_server.py
@@ -71,7 +71,7 @@ def map_one_file(path, n, M, output_path):
 
         output_fname_list.append(fname_out)                             # keep a list of the output txt files to return to the client 
 
-    return output_fname_list
+    return '\n'.join(output_fname_list)
 
 
 def reduce_files(fnames_list, m, output_dir):
@@ -135,10 +135,9 @@ class MapReduceServicer(mapreduce_pb2_grpc.MapReduceServicer):
 
         # Loop through files and perform mapping function:
         for c, file in enumerate(txt_files):
-            output_fname_list = map_one_file(file, c, request.M, request.output_path)
+            out_fname_list = map_one_file(file, c, request.M, request.output_path)
         
-        # Flag finished: 
-        return mapreduce_pb2.OutputPath(path="outputs/intermediate")
+            yield mapreduce_pb2.OutputPath(path = out_fname_list)
         
     def Reduce(self, request, context):
         
@@ -149,11 +148,9 @@ class MapReduceServicer(mapreduce_pb2_grpc.MapReduceServicer):
         for m in range(self.M):
             bucket_files = get_bucket_files(request.input_path, m)
             
-            # Reduce to one file and save in output directory 
+            # Reduce to one file and save in output directory, then report to client
             fname_out = reduce_files(bucket_files, m, request.output_path)
-        
-        # Flag finished: 
-        return mapreduce_pb2.OutputPath(path = request.output_path)
+            yield mapreduce_pb2.OutputPath(path = fname_out)
         
     
 def serve():
